1aec9f462069e1ef31f91766ff5d1632b2b30bf8
von-moyo <florishadekogbe@gmail.com>
2022-06-02 13:30:11 +0000

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * * main - Write '"Programming is like building a multilingual puzzle'
- * *Return: Always 0 (success)
- * */
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
