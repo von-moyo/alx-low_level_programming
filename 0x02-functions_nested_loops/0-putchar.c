0662ab947b5e2635729297694d63299338a34ea1
von-moyo <florishadekogbe@gmail.com>
2022-06-07 14:27:49 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Print "Holberton"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char vibes[] = "_putchar";
+
+	while (i < 8)
+	{
+		putchar(vibes[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
