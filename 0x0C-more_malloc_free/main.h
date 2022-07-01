fe4d40acc50f09ef869dc1d1c9b84c53a2830e38
Adekogbe Florish <68704280+von-moyo@users.noreply.github.com>
2022-07-01 07:29:40 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
-=======
-#ifndef HOLBERTON_FILE
-#define HOLBERTON_FILE
-
-#include <stdlib.h>
-#include <stdio.h>
-
->>>>>>> f6a2f214e250e8ab97d98694eb56dba5094db0bf
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-<<<<<<< HEAD
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-=======
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-#endif
->>>>>>> f6a2f214e250e8ab97d98694eb56dba5094db0bf
