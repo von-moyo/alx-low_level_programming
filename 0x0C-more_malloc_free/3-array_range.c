2f142941aab6aeab67280be6728374756fcff074
Adekogbe Florish <68704280+von-moyo@users.noreply.github.com>
2022-07-01 07:31:23 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdlib.h>
 
 /**
@@ -28,27 +27,4 @@ int *array_range(int min, int max)
 		ptr[i] = min + i;
 
 	return (ptr);
-=======
-#include "main.h"
-
-/**
- * array_range - creates an array of integers
- * @min: minimum
- * @max: maximum
- * Return: array
- */
-int *array_range(int min, int max)
-{
-	int *arr, i = 0, t = min;
-
-	if (min > max)
-		return (0);
-	arr = malloc((max - min + 1) * sizeof(int));
-
-	if (!arr)
-		return (0);
-	while (i <= max - min)
-		arr[i++] = t++;
-	return (arr);
->>>>>>> f6a2f214e250e8ab97d98694eb56dba5094db0bf
 }
