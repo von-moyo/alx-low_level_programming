2e80973753a33f839e7a39ccd65d6aec8e397701
Adekogbe Florish <68704280+von-moyo@users.noreply.github.com>
2022-07-01 07:43:46 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
@@ -179,34 +178,4 @@ void print_result(char *src, int length)
 		if (src[i] >= '0' && src[i] <= '9')
 		printf("%c", src[i]);
 	}
-=======
-#include "main.h"
-
-/**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
-int main(int argc, char *argv[])
-{
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
-
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
->>>>>>> f6a2f214e250e8ab97d98694eb56dba5094db0bf
 }
